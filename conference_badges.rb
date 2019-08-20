e00c5612e519b99f76a4f4ba886cb12c5617418b
Roberto Medina <robby.medina562@gmail.com>
2019-08-20 19:09:52 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,4 @@
-require 'pry'
+
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
@@ -10,7 +10,7 @@ end
 def assign_rooms(guests)
   guests.map.with_index {|name, room| p "Hello, #{name}! You'll be assigned to room #{room+1}!"}
 end
-
+require 'pry'
 def printer(attendees)
  puts batch_badge_creator(attendees){|guests|}
 end
