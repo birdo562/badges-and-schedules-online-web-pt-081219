6dca2e3e1ec673cb8231fc1b43eaf386a3d5545c
Roberto Medina <robby.medina562@gmail.com>
2019-08-20 19:02:22 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+name_array.map {|attendees| badge_maker(attendees)}
+end
+
+def assign_rooms(guests)
+  guests.map.with_index {|name, room| p "Hello, #{name}! You'll be assigned to room #{room+1}!"}
+end
+
+def printer(attendees)
+ puts batch_badge_creator(attendees){|guests|}
+ binding.pry
+ 
+end
